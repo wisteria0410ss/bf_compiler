e18941389bcdd319544e3762bf82633550871dc6
wisteria0410ss <wisteria0410ss@gmail.com>
2019-11-19 02:05:27 +0900

    main 変更

--- src/main.c
+++ src/main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include "containers.h"
 #include "tokenizer.h"
+#include "parser.h"
 
 int main(){
     string *source, *tokens;
+    code_tree *tree;
     char c;
 
     source = string_init();
 
     while((c = fgetc(stdin)) != EOF) string_push(source, c);
     tokens = tokenize(source);
+    tree = parse(tokens);
 
     printf("%s\n", tokens->buf);
 
